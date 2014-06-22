1defb9a51d64e2363fb982f8fc48de9c78b83e43
Jean THOMAS <contact@tibounise.com>
2014-06-22 12:27:58 +0200

    Updated testapi.php to allow us to test multilines messages

--- testapi.php
+++ testapi.php
@@ -4,6 +4,6 @@ require 'config.php';
 require 'FreemobileNotificationSender.php';
 
 $fms = new FreemobileNotificationSender(USERID,APIKEY);
-$fms->sendMessage(' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar metus purus, eu adipiscing tortor condimentum ut. Maecenas eget elementum nisi volutpat');
-
+$fms->sendMessage('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar metus purus, eu adipiscing tortor condimentum ut. Maecenas eget elementum nisi volutpat');
+$fms->sendMessage("First line\nSecond line\nThird line");
 ?>
